bd5768c1ceb3c07ff7c7cb7e3e952fe171fd10ab
Md Mashrafi Rahman <mashrafirahman2024@gmail.com>
2026-01-11 11:01:20 +0600

    Add string comparison example in Java
    Demonstrate string comparison in Java using equals() and '==' operator.

---
+++ 34. Output of Given String Code.java
@@ -0,0 +1,15 @@
+String s1 = "This is ICT 2107 Java";
+String s2 = new String("This is ICT 2107 Java");
+String s3 = "This is ICT 2107 Java";
+
+System.out.println(s1.equals(s2));
+System.out.println(s1 == s2);
+System.out.println(s1 == s3);
+
+
+
+Output
+  
+true
+false
+true
